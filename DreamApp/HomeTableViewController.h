c8ebf3f7d782cedaacbdf7644c4a5445f6d5ba7e
lokada <lyokada@ucsc.edu>
2014-10-06 11:03:58 -0700

    navigation fixed for home view

---
+++ DreamApp/HomeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeTableViewController.h
+//  DreamApp
+//
+//  Created by Lynne Okada on 10/6/14.
+//  Copyright (c) 2014 Lynne Okada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableViewController : UITableViewController
+
+@end
